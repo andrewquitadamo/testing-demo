cb49e9dc6a98b1c175a6ad9b3e10b56bbff4ff02
Andrew Quitadamo <aquitada@uncc.edu>
2016-04-04 07:16:16 -0400

    Fixed function name

--- test_gc.py
+++ test_gc.py
@@ -5,9 +5,9 @@ class GcTests(unittest.TestCase):
     def test_gc(self):
         self.assertEqual(gc('ACTG'), 0.5)
     def test_empty(self):
-        self.assertRaises(TypeError, gc3, )
+        self.assertRaises(TypeError, gc, )
 #    def test_long(self):
-#        self.assertAlmostEqual(gc3('ACTGCAGATCTGAAATTCAGTAAGGG'), 0.4230769)
+#        self.assertAlmostEqual(gc('ACTGCAGATCTGAAATTCAGTAAGGG'), 0.4230769)
 
 if __name__ == '__main__':
     unittest.main()
